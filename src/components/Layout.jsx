21faf9a5e065d97a27c6fb2131fd9da7fa0a5c46
HanielOntiveros <haniel97@hotmail.com>
2024-02-08 16:24:28 -0600

    Adding Link to the layout component

---
+++ src/components/Layout.jsx
@@ -0,0 +1,27 @@
+import { Link, Outlet, useLocation } from 'react-router-dom'
+
+export const Layout = () => {
+    const location = useLocation()
+    console.log(location);
+
+    return (
+        <div className='md:flex md:min-h-screen '>
+            <aside className='md:w-1/4 bg-blue-900 px-5 py-10'>
+                <h2 className='text-4xl font-black text-center text-white'>CRM Cliente</h2>
+                <nav className='mt-10'>
+                    <ul>
+                        <li >
+                            <Link className={`${location.pathname === '/' ? 'text-blue-300 font-bold text-3xl' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`} to={'/'}>Cliente</Link>
+                        </li>
+                        <li>
+                            <Link className={`${location.pathname === '/clientes/nuevo' ? 'text-blue-300 font-bold text-3xl' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`} to={'/clientes/nuevo'}>Nuevos clientes</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </aside>
+            <main className='md:w-3/4 p-10 md:h-screen overflow-scroll'>
+                <Outlet />
+            </main>
+        </div >
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from './App.jsx'
+
+import ErrorPage from './ErrorPage.jsx';
+import { Layout } from './components';
+import { Index, NuevoCliente } from './pages';
+
 import './index.css'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Index />
+      },
+      {
+        path: '/clientes/nuevo',
+        element: <NuevoCliente />
+      }
+    ]
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-
   </React.StrictMode>,
 )
